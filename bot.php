d0c25e9e2e0fe8b134c955e912d0b76f6d2bcb09
wuttinanTH <wuttinan.wch@gmail.com>
2017-05-17 16:32:48 +0700

    update

--- bot.php
+++ bot.php
@@ -30,13 +30,16 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+            $proxy = 'velodrome.usefixie.com:80';
+            $proxyauth = 'fixie:PWFuWJ1klFnk1BE';  
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
--- verify.php
+++ verify.php
@@ -1,14 +1,20 @@
 <?php
 $access_token = 'ChcuM/V/vEUT7zqMpZk9Z1U5nsHcUNaL2ClUUaCW2X4bQu7NjNWXwGO2gUw0pH7Lg7XO5yvDYmOXw64ux4fanfOuX6YkcTtfV8c1BPxbKrmo9BMLA5R5aerW7tHX3PvxMmlW9FvMVrNLag1h4uxCAAdB04t89/1O/w1cDnyilFU=';
 
+
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
+
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:PWFuWJ1klFnk1BE';
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
